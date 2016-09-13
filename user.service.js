29d201535f09a972a9745c17fdad5a007e4a696b
Victor Mitin <lolwner@gmail.com>
2016-09-13 22:50:49 +0300

    Delete user.service.js

--- user.service.js
+++
@@ -1,8 +0,0 @@
-angular.module('test_app')
-    .service('User', function () {
-        var ID = 1;
-        this.name = 'Admin-service';
-        this.ID = function(){
-            return ID;
-        };
-    });
