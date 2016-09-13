99a010f60823fc868e0407b2730dab28b4ffa1d3
Victor Mitin <lolwner@gmail.com>
2016-09-13 22:50:12 +0300

    Delete header.controller.js

--- header.controller.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    'use stict';
-angular.module('test_app').controller('HeaderController', function ($scope, User) {
-    $scope.Name = User.name; //using service
-    $scope.ID = User.ID();
-});
-// rootscope - global var, bad
-
-})();
